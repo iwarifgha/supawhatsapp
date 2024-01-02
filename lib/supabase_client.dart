85c0ee82074d1e5507d6fb6cb23868d2bdfd1988
Iwari <iwaritobyfgha@gmail.com>
2024-01-02 18:24:45 +0100

    second commit

--- lib/supabase_client.dart
+++ lib/supabase_client.dart
@@ -1,11 +1,22 @@
 
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'config.dart';
+
+
+/*
+Uncomment this class and place your own 'url' and 'anonKey' from your own supabase project here.
+
+class Config {
+  static const kUrl = 'YOUR_SUPABASE_URL';
+  static const kAnonKey = 'YOUR SUPABASE ANON KEY';
+ }*/
+
 class MySupabaseClient {
   static Future<void> initialize() async {
     await Supabase.initialize(
-        url: 'https://klbphmfsiwvrrchuevwi.supabase.co',
-        anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsYnBobWZzaXd2cnJjaHVldndpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg4NTEyNDYsImV4cCI6MjAxNDQyNzI0Nn0.iZw6jAWfXytMeWh_DYiJ-dDYUkzCsXpAGKVMo0w8qTk'
+        url: Config.kUrl ,
+        anonKey: Config.kAnonKey,
     );
   }
 
