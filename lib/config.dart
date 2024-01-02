c7d968658c3f8295403a1551502d5b799f55b830
Iwari <iwaritobyfgha@gmail.com>
2024-01-02 18:45:19 +0100

    fix

--- lib/config.dart
+++
@@ -1,5 +0,0 @@
-
-class Config {
-  static const kUrl = 'https://klbphmfsiwvrrchuevwi.supabase.co';
-  static const kAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsYnBobWZzaXd2cnJjaHVldndpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg4NTEyNDYsImV4cCI6MjAxNDQyNzI0Nn0.iZw6jAWfXytMeWh_DYiJ-dDYUkzCsXpAGKVMo0w8qTk';
-}
